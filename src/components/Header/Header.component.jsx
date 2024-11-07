7c16f690fe69623a5024d5227e895be206a81300
Arbab Hussain <38646088+arbabhsiddiqui@users.noreply.github.com>
2024-11-07 20:59:54 +0530

    add header inner element

--- src/components/Header/Header.component.jsx
+++ src/components/Header/Header.component.jsx
@@ -1,4 +1,10 @@
-import { Layout, Breadcrumb, Flex } from 'antd';
+import { Layout, Breadcrumb, Space, Input, Avatar, Badge } from 'antd';
+import {
+  SearchOutlined,
+  UserOutlined,
+  SettingFilled,
+  BellFilled,
+} from '@ant-design/icons';
 
 // local imports
 const { Header: AntHeader } = Layout;
@@ -6,6 +12,8 @@ const { Header: AntHeader } = Layout;
 import './Header.style.scss';
 
 const Header = () => {
+  const handleSignIn = () => {};
+
   return (
     <AntHeader className='c-header'>
       <div className='c-header__title-area'>
@@ -22,7 +30,36 @@ const Header = () => {
         <h3>Dashboard</h3>
       </div>
 
-      <Flex></Flex>
+      <div className='c-header__profile-area'>
+        <Space.Compact className='search'>
+          <Input
+            addonBefore={<SearchOutlined />}
+            placeholder='Search'
+          />
+        </Space.Compact>
+
+        <div
+          className='sign-in'
+          onClick={handleSignIn}
+        >
+          <Avatar
+            className='sign-avatar'
+            size={20}
+            icon={<UserOutlined />}
+          />{' '}
+          Sign in
+        </div>
+        <SettingFilled className='hover' />
+        <Badge
+          className='hover'
+          count={4}
+        >
+          <Avatar
+            className='bell-avatar'
+            icon={<BellFilled />}
+          />
+        </Badge>
+      </div>
     </AntHeader>
   );
 };
