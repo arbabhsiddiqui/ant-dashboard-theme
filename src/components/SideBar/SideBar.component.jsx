486656e197986e6da7e794805ad4c63c16a52df8
Arbab Hussain <38646088+arbabhsiddiqui@users.noreply.github.com>
2024-11-07 09:27:39 +0530

    setup collapsible sidebar

--- src/components/SideBar/SideBar.component.jsx
+++ src/components/SideBar/SideBar.component.jsx
@@ -1,4 +1,6 @@
-import { Layout } from 'antd';
+import { useState } from 'react';
+import { Button, Layout } from 'antd';
+import { MenuFoldOutlined, MenuUnfoldOutlined } from '@ant-design/icons';
 
 // local imports
 const { Sider } = Layout;
@@ -6,7 +8,21 @@ const { Sider } = Layout;
 import './SideBar.style.scss';
 
 const SideBar = () => {
-  return <Sider className='c-sidebar'>SideBar</Sider>;
+  const [collapsed, setCollapsed] = useState(false);
+
+  return (
+    <Sider
+      // collapsible
+      collapsed={collapsed}
+      className='c-sidebar'
+    >
+      <Button
+        icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+        onClick={() => setCollapsed(!collapsed)}
+        SideBar
+      ></Button>
+    </Sider>
+  );
 };
 
 export default SideBar;
